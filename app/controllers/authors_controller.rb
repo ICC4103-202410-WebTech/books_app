3bbabd5986557f1f1e4773726b733b2805658559
MaximilianoGarciaRoe <mgarcia@buk.cl>
2024-04-28 20:11:11 -0400

    feat: add crete authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,23 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find_by(id: params[:id])
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    author = Author.new(author_params)
+    if author.save
+      redirect_to authors_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :age)
+  end
 end
