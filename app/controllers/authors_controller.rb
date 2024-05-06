07a05cda1a59d300942d3a6e5ce1ab782cc6d471
MaximilianoGarciaRoe <mgarcia@buk.cl>
2024-05-06 08:34:08 -0400

    feat(class 08): add create, edit and delete authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,45 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find_by(id: params[:id])
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    author = Author.new(author_params)
+
+    if author.save!
+      redirect_to authors_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @author = Author.find_by(id: params[:id])
+  end
+  
+  def update
+    puts "params: #{params}"
+    author = Author.find_by(id: params[:id])
+
+    if author.update!(author_params)
+      redirect_to authors_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    author = Author.find_by(id: params[:id])
+    author.destroy
+    redirect_to authors_path
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :age)
+  end
 end
